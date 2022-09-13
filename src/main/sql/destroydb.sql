96201ff261882ac414f8a3a61c7ef1c032c1f3c1
aljsanchez <aljsanchez@gmail.com>
2022-09-13 08:29:06 -0300

    - Migración a flyway

--- src/main/sql/destroydb.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE public.performance;
-DROP SEQUENCE public.performance_sq;
-DROP TABLE public.bordero;
-DROP SEQUENCE public.bordero_sq;
-DROP TABLE public.customer;
-DROP SEQUENCE public.customer_sq;
-DROP TABLE public.play;
-DROP SEQUENCE public.play_sq;
--- src/main/sql/populate.sql
+++
@@ -1,4 +0,0 @@
-\i populate_play.sql
-\i populate_customer.sql
-\i populate_bordero.sql
-\i populate_performance.sql
